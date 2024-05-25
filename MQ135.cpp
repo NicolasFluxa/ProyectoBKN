dc4f4b1733955b61371907f0a45a49f8a8dda771
NicolasFluxa <fluxa.andres@gmail.com>
2024-05-25 17:12:49 -0400

    Algo 2,0

--- MQ135.cpp
+++ MQ135.cpp
@@ -1,27 +1,16 @@
-#include <MQUnifiedsensor.h>
+#include <Wire.h>
 
-// Define los pines de conexión
-String pin = "32"; //Define el pin analógico donde se conectará el sensor
-String type = "MQ-135"; //Define el tipo de sensor MQ
-float voltage_Resolution = 3.3; //La resolución de tu ADC
-int ADC_Bit_Resolution = 10; //La resolución de tu ADC
-int RatioMQ135CleanAir = 3.6; //RS/RO esta es la relación del sensor en aire limpio
-
-MQUnifiedsensor MQ135("Arduino", voltage_Resolution, ADC_Bit_Resolution, pin.toInt(), type);
+#define MQ135_PIN 15 // Conecta tu MQ135 a la entrada analógica A0
 
 void setup() {
-  Serial.begin(115200);
-  MQ135.init();
+  Serial.begin(9600);
+
 }
 
 void loop() {
-  float calcRatio = MQ135.readSensor(); //Lee el sensor y obtiene la relación RS/RO
-  Serial.print("Ratio: ");
-  Serial.println(calcRatio);
-
-  float calcGas = MQ135.readSensor(); //Lee el sensor y calcula la concentración de gas
-  Serial.print("Concentration: ");
-  Serial.println(calcGas);
+  int mq135_value = analogRead(MQ135_PIN)/4;
+  Serial.print("MQ135 Value: ");
+  Serial.println(mq135_value);
 
   delay(1000);
 }
